9ea27af7a4b20581c3c9073aac3c0d1ac2bf519d
Dinesh Nanayakkara <dineshucsc@gmail.com>
2017-10-02 12:06:55 +0530

    Initial commit

---
+++ BAL/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibryManagement.BAL
+{
+    class User
+    {
+        private static int userSize;
+        private int userId;
+        private String userName;
+        private String password;
+
+
+
+        public void User(int id)
+        {
+            this.userId = id;
+            userSize += 1;
+        }
+
+        public void RemoveUser(int id) //return type boolean
+        {
+            //TODO: Call the Remove procedure and return the value is true or false 
+        }
+
+        public void Login(String uId, String pwd) // return type boolean
+        {
+            //TODO: Call Login Procedure and return the value is login sucess or false
+        }
+
+        public void Login2(String uId, String pwd) // return type boolean
+        {
+            //TODO: Call Login Procedure and return the value is login sucess or false
+        }
+
+       
+    }
+}
